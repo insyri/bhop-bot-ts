a143f27ec35bd997819d3edb5938b1b9ce68d833
insyri <60794909+insyri@users.noreply.github.com>
2021-11-10 20:48:05 -0500

    rps done EZ

--- src/commands/rps.ts
+++ src/commands/rps.ts
@@ -2,15 +2,59 @@ import { CommandClient } from "detritus-client";
 import { BaseCommand } from "../basecommand";
 import { firstUppercase } from "../utils";
 
-type RPSInputs = "rock" | "paper" | "scissors" | "scissor";
+type RPSInputs = "rock" | "paper" | "scissors";
 type RPSEmojis = "👊" | "🖐" | "✌";
 
 let RPSEmojisMap = new Map<RPSInputs, RPSEmojis>();
 
 RPSEmojisMap.set("rock", "👊");
 RPSEmojisMap.set("paper", "🖐");
-RPSEmojisMap.set("scissor", "✌");
 RPSEmojisMap.set("scissors", "✌");
+
+interface evalRpsResult {
+  returning: "It's a draw!" | "wins" | "loses" | "bruh";
+  whoWon: "bot" | "user" | null;
+}
+
+function evalRps(
+  userInput: RPSInputs,
+  botInput: RPSInputs
+): evalRpsResult | null {
+  let win: evalRpsResult = {
+    returning: "wins",
+    whoWon: "user",
+  };
+  let lose: evalRpsResult = {
+    returning: "loses",
+    whoWon: "bot",
+  };
+
+  if (userInput === botInput)
+    return {
+      returning: "It's a draw!",
+      whoWon: null,
+    };
+  if (userInput === "rock") {
+    if (botInput === "paper") {
+      return lose;
+    } else {
+      return win;
+    }
+  } else if (userInput === "paper") {
+    if (botInput === "scissors") {
+      return lose;
+    } else {
+      return win;
+    }
+  } else if (userInput === "scissors") {
+    if (botInput === "rock") {
+      return lose;
+    } else {
+      return win;
+    }
+  }
+  return null;
+}
 
 export default class RockPaperscissors extends BaseCommand {
   constructor(client: CommandClient) {
@@ -21,25 +65,24 @@ export default class RockPaperscissors extends BaseCommand {
       metadata: {
         description: "Play Rock Paper Scissors.",
       },
-      run: async (ctx, args: { _rps: RPSInputs }) => {
-        if (args._rps === undefined)
-          return await ctx.reply("You need to choose a decision.");
-          
-        // Maybe make BaseMap extend Map?
-        // https://github.com/detritusjs/utils/blob/master/src/baseset.ts
-        
-        // Make rps array of RPSEmojisMap values
-        let rps;
-        //let rps = RPSEmojisMap.values().
-
-        if (rps.some((x) => x === args._rps)) {
-
-          let rnd: number = Math.floor(Math.random() * rps.length);
-          let botchoice = rps[rnd];
-          console.log(botchoice, "ok");
+      run: async (ctx, args: { rps: RPSInputs }) => {
+        if (!args.rps) return await ctx.reply("You need to choose a decision.");
+        let rps_ = Array.from(RPSEmojisMap.keys());
+        if (rps_.some((x) => x === args.rps)) {
+          let rnd: number = Math.floor(Math.random() * rps_.length);
+          let botchoice = rps_[rnd]!;
+          let outcome = evalRps(args.rps, botchoice);
           let result = `[**RPS**] I'm choosing **${firstUppercase(
             botchoice
-          )}**! ${RPSEmojisMap.} **${"test"}** wins!`;
+          )}**! ${RPSEmojisMap.get(botchoice)} ${
+            outcome?.returning === "It's a draw!"
+              ? outcome
+              : `**${
+                  outcome?.whoWon! === "bot"
+                    ? botchoice.toUpperCase()
+                    : args.rps.toUpperCase()
+                }** ${outcome?.returning}`
+          }!`;
           await ctx.reply(result);
         } else ctx.reply("Please choose a valid option.");
       },
