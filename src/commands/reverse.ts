11e55583e3286eeec12fc0b9bed73b5cf9c5d370
insyri <60794909+insyri@users.noreply.github.com>
2021-11-10 20:57:16 -0500

    reverse command

---
+++ src/commands/reverse.ts
@@ -0,0 +1,22 @@
+import { CommandClient } from "detritus-client";
+import { BaseCommand } from "../basecommand";
+
+export default class Reverse extends BaseCommand {
+  constructor(client: CommandClient) {
+    super(client, {
+      name: "reverse",
+      help: "Flip words in the reverse.",
+      //args: [{ name: "decision", label: "decision", required: true }],
+      metadata: {
+        description: "Flip words in the reverse.",
+      },
+      run: async (ctx, args: { reverse: string }) => {
+        if (!args.reverse) return await ctx.reply("no param");
+        await ctx.reply(
+          `[**Reverse**] 🙃 ${args.reverse.split("").reverse().join("")}`
+        );
+        console.log(args.reverse.split("").reverse().join(""));
+      },
+    });
+  }
+}
