f8884025fb26751817e12947f72f2922343e50f8
insyri <60794909+insyri@users.noreply.github.com>
2021-11-25 18:11:33 -0500

    simplified basecommand

--- src/basecommand.ts
+++ src/basecommand.ts
@@ -1,23 +1,8 @@
 import { Command } from "detritus-client";
 import { ParsedArgs } from "detritus-client/lib/command";
 
-export class BaseCommand<
-  ParsedArgsFinished = Command.ParsedArgs
-> extends Command.Command<ParsedArgsFinished> {
+export class BaseCommand extends Command.Command {
   async onRunError(ctx: Command.Context, args: ParsedArgs, error: any) {
     ctx.reply(`An error occurred: ${error}\nWith these arguments: ${args}`);
-  }
-
-  onTypeError(
-    ctx: Command.Context,
-    args: ParsedArgsFinished,
-    errors: Command.ParsedErrors
-  ) {
-    ctx.reply(
-      `An error occurred: ${
-        Array.isArray(errors) ? errors.join("\n") : errors
-      }\nWith these arguments: ${args}`
-    );
-    console.log(errors, args);
   }
 }
