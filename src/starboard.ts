b5311ac9e763437d45fe8b55b7e3709f2b05b559
insyri <60794909+insyri@users.noreply.github.com>
2022-01-06 18:20:40 -0500

    something i forgot

--- src/starboard.ts
+++ src/starboard.ts
@@ -1,28 +1,23 @@
-import { client } from "./index";
+// import { client } from "./index";
 
-async function evalStars(count: number): Promise<5 | 4 | 3 | 2 | 1> {
-  // 7 or more votes, 1
-  // 30 or more votes, 2
-  // 70 or more votes, 3
-  // 100 or more votes, 4
-  // 150 or more votes, 5
+// (async () => {
+//   const starboard = await client.rest
+//     .fetchGuild("759174794968301569")
+//     .then((guild) =>
+//       guild
+//         .fetchChannels()
+//         .then((channels) =>
+//           channels.find((channel) => channel.name === "starboard")
+//         )
+//     );
 
-  if (count >= 7) return 1;
-  else if (count >= 30) return 2;
-  else if (count >= 70) return 3;
-  else if (count >= 100) return 4;
-  else if (count >= 150) return 5;
-  else return 5;
-}
+//   client.on("messageReactionAdd", async (payload) => {
+//     console.log("messageReactionAdd");
+//     if (!payload.message?.fromUser) return;
+//     if (payload.reaction.emoji.name !== "⭐") return;
+//     // if (payload.reaction.count !== 10) return;
 
-client.on("messageReactionAdd", async (payload) => {
-  if (payload.reaction.emoji.name === "⭐") {
-    let ct = payload.reaction.count;
-
-    // Evaluate level, 5 highest and 1 lowest.
-    let level = await evalStars(ct);
-
-    // Update starboard
-    await payload.message?.reply(level.toString());
-  }
-});
+//     // Update starboard
+//     await client.rest.createMessage(starboard!.id, `hi`);
+//   });
+// })();
